6d89e6c1f408ae0d2b8a49e4911c65fc89c54b23
ryanmio <ryan@mioduski.us>
2024-12-29 10:26:13 -0500

    fix: remove typeRoots to fix components type error

---
+++ @types/components/document-view.d.ts
@@ -0,0 +1,32 @@
+interface Recipient {
+  id: string;
+  name: string;
+  email: string;
+  status: string;
+}
+
+interface Document {
+  documentId: string;
+  name: string;
+}
+
+interface Envelope {
+  id: string;
+  subject: string;
+  message: string;
+  status: string;
+  created_at: string;
+  updated_at: string;
+  completed_at?: string;
+  recipients: Recipient[];
+}
+
+interface DocumentViewProps {
+  envelopeId: string;
+  envelope: Envelope;
+  documents: { envelopeDocuments: Document[] };
+  showActions?: boolean;
+}
+
+declare const DocumentView: React.FC<DocumentViewProps>;
+export default DocumentView; 
