cdfef2661d29257016efd85904ffd944efb86013
ryanmio <ryan@mioduski.us>
2025-01-05 18:56:47 -0500

    fix: Update CSP to allow Supabase auth endpoint

--- next.config.js
+++ next.config.js
@@ -14,17 +14,18 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.docusign.com https://*.docusign.net https://docucdn-a.akamaihd.net https://docutest-a.akamaihd.net https://unpkg.com",
-              "style-src 'self' 'unsafe-inline' https://*.docusign.com https://*.docusign.net https://docucdn-a.akamaihd.net",
-              "img-src 'self' data: blob: https://*.docusign.com https://*.docusign.net https://docucdn-a.akamaihd.net",
-              "connect-src 'self' https://*.docusign.com https://*.docusign.net https://docucdn-a.akamaihd.net https://unpkg.com",
-              "frame-src 'self' https://*.docusign.com https://*.docusign.net https://docucdn-a.akamaihd.net",
-              "frame-ancestors 'self' https://*.docusign.com https://*.docusign.net https://docucdn-a.akamaihd.net",
-              "worker-src 'self' blob: https://*.docusign.com https://*.docusign.net https://docucdn-a.akamaihd.net https://docutest-a.akamaihd.net",
-              "child-src 'self' blob: https://*.docusign.com https://*.docusign.net https://docucdn-a.akamaihd.net"
-            ].join('; ')
-          }
-        ]
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.docusign.net https://*.docusign.com https://unpkg.com https://docucdn-a.akamaihd.net",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src 'self' data: blob: https://*.docusign.net https://*.docusign.com",
+              "font-src 'self' data:",
+              "frame-src 'self' https://*.docusign.net https://*.docusign.com",
+              "frame-ancestors 'self' https://*.docusign.net https://*.docusign.com",
+              "connect-src 'self' https://*.docusign.net https://*.docusign.com https://*.supabase.co wss://*.supabase.co https://unpkg.com https://ahynitsoacoisrgodqqq.supabase.co",
+              "worker-src 'self' blob:",
+              "child-src 'self' blob:",
+            ].join('; '),
+          },
+        ],
       }
     ];
   }
