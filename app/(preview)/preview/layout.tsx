8897ff7d03e1453fef251584f22648aef1c989f6
ryanmio <ryan@mioduski.us>
2025-01-06 18:50:25 -0500

    fix: remove redundant header from preview page

--- app/(preview)/preview/layout.tsx
+++ app/(preview)/preview/layout.tsx
@@ -5,18 +5,6 @@ export default function PreviewLayout({
 }) {
   return (
     <div className="min-h-screen bg-[#F8F3F0]">
-      <header className="sticky top-0 z-50 w-full border-b bg-white">
-        <div className="container flex h-14 items-center">
-          <div className="mr-4 hidden md:flex">
-            <a className="mr-6 flex items-center space-x-2" href="/">
-              <span className="hidden font-bold text-[#130032] sm:inline-block">
-                DocuSign Component Preview
-              </span>
-            </a>
-          </div>
-        </div>
-      </header>
-
       <div className="container mx-auto py-8">
         <div className="flex flex-col space-y-8">
           <div className="flex-1 space-y-4">
